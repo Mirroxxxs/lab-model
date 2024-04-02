d1584b74a8b22bb1b8a340ecfc20d11a6b1309d0
GlazovEgor <147654835+Mirroxxxs@users.noreply.github.com>
2024-04-02 14:51:16 +0300

    Add files via upload

---
+++ pract11/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace pract11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Day = new System.Windows.Forms.TextBox();
+            this.Month = new System.Windows.Forms.TextBox();
+            this.Year = new System.Windows.Forms.TextBox();
+            this.append = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Day
+            // 
+            this.Day.Location = new System.Drawing.Point(64, 17);
+            this.Day.Name = "Day";
+            this.Day.Size = new System.Drawing.Size(100, 20);
+            this.Day.TabIndex = 0;
+            // 
+            // Month
+            // 
+            this.Month.Location = new System.Drawing.Point(64, 58);
+            this.Month.Name = "Month";
+            this.Month.Size = new System.Drawing.Size(100, 20);
+            this.Month.TabIndex = 1;
+            // 
+            // Year
+            // 
+            this.Year.Location = new System.Drawing.Point(64, 99);
+            this.Year.Name = "Year";
+            this.Year.Size = new System.Drawing.Size(100, 20);
+            this.Year.TabIndex = 2;
+            // 
+            // append
+            // 
+            this.append.Location = new System.Drawing.Point(35, 125);
+            this.append.Name = "append";
+            this.append.Size = new System.Drawing.Size(129, 37);
+            this.append.TabIndex = 3;
+            this.append.Text = "Добавить";
+            this.append.UseVisualStyleBackColor = true;
+            this.append.Click += new System.EventHandler(this.append_Click);
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(206, 28);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(251, 162);
+            this.Result.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.append);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Year);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Month);
+            this.groupBox1.Controls.Add(this.Day);
+            this.groupBox1.Location = new System.Drawing.Point(21, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(179, 168);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Дата";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Год";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Месяц";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "День";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Result);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(183, 111);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(470, 209);
+            this.panel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Day;
+        private System.Windows.Forms.TextBox Month;
+        private System.Windows.Forms.TextBox Year;
+        private System.Windows.Forms.Button append;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ pract11/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace pract11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         internal class Date
+         {
+            int day;
+            int month;
+            int year;
+            public Date(int day, int month, int year)
+            {
+                this.day = day;
+                this.month = month;
+                this.year = year;
+            }
+            public string GetInformation()
+            {
+                string information;
+                information = $"{this.day}.{this.month}.{this.year}";
+                return information;
+            }
+        }
+        private void append_Click(object sender, EventArgs e)
+        {
+            Date new_date;
+            if (Day.Text == ""  || Month.Text == "" || Year.Text == "")
+            {
+                MessageBox.Show("Введите число");
+            }
+            if (Year.Text != "")
+            {
+                if (Year.Text.All(char.IsNumber))
+                {
+                   int yearValue = int.Parse(Year.Text);
+                   if (yearValue > 0)
+                   {
+                        if(Month.Text != "")
+                        {
+                            if(Month.Text.All(char.IsNumber))
+                            {
+                                int monthValue = int.Parse(Month.Text);
+                                if (monthValue >= 1 && monthValue <= 12)
+                                {
+                                    if (Day.Text != "")
+                                    {
+                                        if (Day.Text.All(char.IsNumber))
+                                        {
+                                            int dayValue = int.Parse(Day.Text);
+                                            if (dayValue > 0 && (((monthValue == 1 || monthValue == 3 || monthValue == 5 || monthValue == 7 || monthValue == 8 || monthValue == 10  || monthValue == 11) && dayValue <= 31) ||
+                                         
+                                                ((monthValue == 4 || monthValue == 6 || monthValue == 9 || monthValue == 11) && dayValue <= 30) ||
+                                              ((monthValue == 2 && dayValue <= 29 && yearValue % 4 == 0) || (monthValue == 2 && dayValue <= 28 && yearValue % 4 != 0) )))
+                                            {
+                                                new_date = new Date(Convert.ToInt32(Day.Text), Convert.ToInt32(Month.Text), Convert.ToInt32(Year.Text));
+                                                Result.Text = new_date.GetInformation();
+                                            }
+                                            else
+                                            {
+                                                MessageBox.Show("День не входит в диапазон");
+                                            }
+                                          
+                                        }
+                                        else
+                                        {
+                                            MessageBox.Show("Введите число");
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Месяц не входит в диапазон");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Введите число");
+                            }
+                        }
+                   }
+                   else
+                   {
+                        MessageBox.Show("Год не входит в диапазон");
+                   }
+                }
+                else
+                {
+                    MessageBox.Show("Введите число");
+                }
+            }
+        }
+    }
+}
